26b572d9619cea4f3189df551bae1e8dd9c3f8c0
Mithilesh Sahni <149224417+mithilesh9135@users.noreply.github.com>
2023-10-29 13:58:30 +0530

    code updated

--- src/StudentInventory.java
+++ src/StudentInventory.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+// this is for edit the code
 import java.sql.*;
 import java.util.Vector;
 import javax.swing.JOptionPane;
